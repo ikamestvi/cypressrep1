4a2608831503a1ea2d9b31806a739472835e80af
ikamestvi <ika.mestvi@gmail.com>
2025-07-17 13:27:58 +0400

    dav2

---
+++ cypress/support/automationTestCommands.js
@@ -0,0 +1,8 @@
+Cypress.Commands.add('loginAuto', (email, password) => { 
+    cy.visit('https://automationteststore.com')
+    cy.get('#customer_menu_top > li > a').click()
+
+    cy.get('[name="loginname"]').clear().type("ika_ika")
+    cy.get('[name="password"]').clear().type('12345678ffrr')
+    cy.get('#loginFrm > fieldset > .btn').click()
+ })
